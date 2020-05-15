72f39957a580c28564a13239203e5e2e409fb82d
liakassardjian <liakassardjian@gmail.com>
2020-05-15 14:57:48 -0300

    Criado projeto do Xcode

---
+++ Kaligo/Kaligo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kaligo
+//
+//  Created by Lia Kassardjian on 15/05/20.
+//  Copyright © 2020 Lia Kassardjian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
