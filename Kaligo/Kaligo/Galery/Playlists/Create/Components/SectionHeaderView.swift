f49eebdf994eb4efc3e2b3a4b3ea2ec0e3181fd3
liakassardjian <liakassardjian@gmail.com>
2020-06-02 16:47:20 -0300

    Inseridas seções nas tables de criação

---
+++ Kaligo/Kaligo/Galery/Playlists/Create/Components/SectionHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  SectionHeaderView.swift
+//  Kaligo
+//
+//  Created by Lia Kassardjian on 02/06/20.
+//  Copyright © 2020 Lia Kassardjian. All rights reserved.
+//
+
+import UIKit
+
+class SectionHeaderView: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
